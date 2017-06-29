31c8a1569322c477dc06861486dad518faf8cf0e
viniciusfigueiredo-linx <vinicius.figueiredo@linx.com.br>
2017-06-29 19:30:15 -0300

    jsdocs start

--- src/lib/clusterizer.js
+++ src/lib/clusterizer.js
@@ -1,6 +1,44 @@
+/**
+ * @author Vinicius Figueiredo <vinifig@hotmail.com>
+ */
 export default class Clusterizer {
-  constructor (id, dependencies, script) {
-    this._id = id;
+  /**
+   * @name constructor
+   *
+   * @param config {Object}
+   *   @required
+   *   @type {Object}
+   *
+   *    @attribute id
+   *      @required
+   *      @description Clusterized process id
+   *
+   *    @attribute devices
+   *      @required
+   *      @description List of Devices to cluster
+   *
+   *    @attribute size
+   *      @description Size of data to cluster. If needed
+   *      @default Infinity
+   **/
+  constructor (config, dependencies, script) {
+    if (! config) {
+      throw new ReferenceError(`Expected 'config' parameter`)
+    }
+    if (! config.id) {
+      throw new ReferenceError(`Expected attribute 'id' of 'config' parameter`)
+    }
+    if (! config.devices) {
+      throw new ReferenceError(`Expected attribute 'devices' of 'config' parameter`)
+    }
+
+    if (! dependencies) {
+      throw new ReferenceError(`Expected attribute 'devices' of 'config' parameter`)
+    }
+
+    this._id = config.id;
+    this._devices = config.devices;
+    this._size = config.size || Infinity;
     this._dependencies = dependencies;
     this._script = script;
   }
